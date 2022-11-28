d394ec3c1a914f84b28f0c93e76b834d2d9f37c2
furkan.kucuk <oiua66f7olp67z3wciqh7tqwi5lvok5342iw7zwrmgp5sacrur3a>
2022-11-28 12:51:20 +0300

    first commit

---
+++ app/src/main/java/com/mfk/changeappicon/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.mfk.changeappicon
+
+import android.content.ComponentName
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnFirst: Button
+    private lateinit var btnSecond: Button
+    private lateinit var btnThird: Button
+    private lateinit var btnFourth: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initialize()
+        handleClickEvents()
+    }
+
+    private fun handleClickEvents() {
+        btnFirst.setOnClickListener {
+        setIcon(ICON_COLOUR.BLUE)
+        }
+        btnSecond.setOnClickListener {
+            setIcon(ICON_COLOUR.RED)
+        }
+        btnThird.setOnClickListener {
+            setIcon(ICON_COLOUR.GREEN)
+        }
+        btnFourth.setOnClickListener {
+
+        }
+    }
+
+    enum class ICON_COLOUR { RED, GREEN, BLUE }
+
+    private fun setIcon(targetColour: ICON_COLOUR) {
+        for (value in ICON_COLOUR.values()) {
+            val action = if (value == targetColour) {
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+            } else {
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+            }
+            packageManager.setComponentEnabledSetting(
+                ComponentName(BuildConfig.APPLICATION_ID, "${BuildConfig.APPLICATION_ID}.${value.name}"),
+                action, PackageManager.DONT_KILL_APP
+            )
+        }
+    }
+
+    private fun initialize() {
+        btnFirst = findViewById(R.id.btnFirst)
+        btnSecond = findViewById(R.id.btnSecond)
+        btnThird = findViewById(R.id.btnThird)
+        btnFourth = findViewById(R.id.btnFourth)
+    }
+
+
+}
